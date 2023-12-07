1bb19b2f4bb4b9e6d0ea640b5e1e9a79f53c940c
Gustavo Krüger de Mira <122942022+GugaKMG@users.noreply.github.com>
2023-12-07 16:26:25 -0300

    Adição dos Concurrency e correção da Atividade_2 de Maps

--- Collections/Map/src/Atividades/Atividade_2.java
+++ Collections/Map/src/Atividades/Atividade_2.java
@@ -9,13 +9,17 @@ public class Atividade_2 {
 	public static void main(String[] args) {
 		try {
 		
+		int verif;
 		String srt1, srt2;
 		
 		Map<String, String> map = new LinkedHashMap<String, String>();
 		Scanner s = new Scanner(System.in);
 		
-		for(int i = 0; i < 4; i++) {
-			System.out.println("Digite duas palavras: ");
+		System.out.println("Quantas verificações você deseja?");
+		verif = s.nextInt();
+		
+		for(int i = 0; i < verif; i++) {
+			System.out.println("Digite duas diferentes palavras: ");
 			srt1 = s.next();
 			srt2 = s.next();
 			
@@ -30,7 +34,12 @@ public class Atividade_2 {
 		
 		for(Map.Entry<String, String> be : map.entrySet()) {
 			if(be.getKey().length() == be.getValue().length()) {
-				System.out.println("A palavra " + be.getKey() + " é tem o mesmo número de letras da palavra " + be.getValue());
+				System.out.println("A palavra " + be.getKey() + " tem o mesmo número de letras da palavra " + be.getValue());
+				System.out.println("----------");
+			}
+			else{
+				System.out.println("A palavra " + be.getKey() + " não tem o mesmo número de letras da palavra " + be.getValue());
+				System.out.println("----------");
 			}
 		}
 		s.close();
